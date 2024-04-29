d7bd0153904b17183cdd8ffc2fd18026cbf201b1
王哲玮 <m13636472690@163.com>
2024-04-29 12:47:30 +0800

    update

--- The Others/Assets/_Game/Player/dimensionChange.cs
+++ The Others/Assets/_Game/Player/dimensionChange.cs
@@ -7,177 +7,28 @@ public class dimensionChange : MonoBehaviour
 {
     public float interactionDistance = 5f;
 
-    public AudioClip enlarge, shrink;
-
-    [SerializeField]
-    private bool canChange = true;
-
     private GameObject obj;
-
-    private Transform tf;
-
-    private float col;
-
     private objectState flag;
 
     void Update()
     {
-        if (canChange)
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactionDistance))
         {
-            Ray ray = new Ray(transform.position, transform.forward);
 
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, interactionDistance))
+            if (hit.collider.gameObject.tag == "object")
             {
-
-                if (hit.collider.gameObject.tag == "object")
+                if (Input.GetMouseButtonDown(1))
                 {
-
                     obj = hit.collider.gameObject;
-
                     flag = obj.GetComponent<objectState>();
 
-                    tf = obj.transform;
-
-                    AudioSource Sound = hit.collider.gameObject.GetComponent<AudioSource>();
-
-                    if (Input.GetMouseButtonDown(1) && canChange)
-                    {
-                        if (flag.ChangeDimensionX)
-                        {
-                            col = obj.GetComponent<Collider>().bounds.size.x;
-                        }
-                        else if (flag.ChangeDimensionY)
-                        {
-                            col = obj.GetComponent<Collider>().bounds.size.y;
-                        }
-                        else if (flag.ChangeDimensionZ)
-                        {
-                            col = obj.GetComponent<Collider>().bounds.size.z;
-                        }
-
-                        print(col);
-
-                        if (!flag.stateIsLarge)
-                        {
-                            canChange = false;
-                            StartCoroutine(Enlarge());
-
-                            Sound.clip = enlarge;
-                            Sound.Play();
-                        }
-                        else
-                        {
-                            canChange = false;
-                            StartCoroutine(Shrink());
-
-                            Sound.clip = shrink;
-                            Sound.Play();
-                        }
-
-
-                    }
+                    flag.try2invoke();
                 }
             }
         }
     }
-
-    IEnumerator Shrink()
-    {
-        if (flag.ChangeDimensionX)
-        {
-            var cnt = 0;
-            while (col > 0.1 && cnt <= 1000)
-            {
-                cnt++;
-                col = obj.GetComponent<Collider>().bounds.size.x;
-                print(col + obj.name);
-                tf.localScale = new Vector3(tf.localScale.x * 0.99f, tf.localScale.y, tf.localScale.z);
-                yield return new WaitForSeconds(0.008f);
-            }
-            flag.stateIsLarge = false;
-            canChange = true;
-            print("Shrink Finished");
-        }
-        else if (flag.ChangeDimensionY)
-        {
-            var cnt = 0;
-            while (col > 0.1 && cnt <= 1000)
-            {
-                cnt++;
-                col = obj.GetComponent<Collider>().bounds.size.y;
-                print(col + obj.name);
-                tf.localScale = new Vector3(tf.localScale.x, tf.localScale.y * 0.99f, tf.localScale.z);
-                yield return new WaitForSeconds(0.008f);
-            }
-            flag.stateIsLarge = false;
-            canChange = true;
-            print("Shrink Finished");
-        }
-        else if (flag.ChangeDimensionZ)
-        {
-            var cnt = 0;
-            while (col > 0.1 && cnt <= 1000)
-            {
-                cnt++;
-                col = obj.GetComponent<Collider>().bounds.size.z;
-                print(col + obj.name);
-                tf.localScale = new Vector3(tf.localScale.x, tf.localScale.y, tf.localScale.z * 0.99f);
-                yield return new WaitForSeconds(0.008f);
-            }
-            flag.stateIsLarge = false;
-            canChange = true;
-            print("Shrink Finished");
-        }
-        
-    }
-
-    IEnumerator Enlarge()
-    {
-        if (flag.ChangeDimensionX)
-        {
-            var cnt = 0;
-            while (tf.localScale.x < 1 && cnt <= 1000)
-            {
-                cnt++;
-                tf = obj.transform;
-                tf.localScale = new Vector3(tf.localScale.x * 1.05f, tf.localScale.y, tf.localScale.z );
-                yield return new WaitForSeconds(0.008f);
-            }
-            flag.stateIsLarge = true;
-            canChange = true;
-            print("Enlarge Finished");
-        }
-        else if (flag.ChangeDimensionY)
-        {
-            var cnt = 0;
-            while (tf.localScale.y < 1 && cnt <= 1000)
-            {
-                cnt++;
-                tf = obj.transform;
-                tf.localScale = new Vector3(tf.localScale.x, tf.localScale.y * 1.05f, tf.localScale.z );
-                yield return new WaitForSeconds(0.008f);
-            }
-            flag.stateIsLarge = true;
-            canChange = true;
-            print("Enlarge Finished");
-        }
-        else if (flag.ChangeDimensionZ)
-        {
-            var cnt = 0;
-            while (tf.localScale.z < 1 && cnt <= 1000)
-            {
-                cnt++;
-                tf = obj.transform;
-                tf.localScale = new Vector3(tf.localScale.x, tf.localScale.y, tf.localScale.z * 1.05f);
-                yield return new WaitForSeconds(0.008f);
-            }
-            flag.stateIsLarge = true;
-            canChange = true;
-            print("Enlarge Finished");
-        }
-
-    }
-
 }
--- The Others/Assets/_Game/objectState.cs
+++ The Others/Assets/_Game/objectState.cs
@@ -7,5 +7,54 @@ public class objectState : MonoBehaviour
     public bool stateIsLarge = true;
     public bool AllowToHold = true;
     public bool AllowToChangeDimension = true;
-    public bool ChangeDimensionX = false, ChangeDimensionY = false, ChangeDimensionZ = false;
+
+    private Transform tf;
+    private Collider col;
+    private bool canChange = true;
+
+    public void try2invoke()
+    {
+        col = GetComponent<Collider>();
+        if (canChange)
+        {
+            if (stateIsLarge)
+            {
+                StartCoroutine(Shrink());
+            }
+            else
+            {
+                StartCoroutine(Enlarge());
+            }
+            canChange = false;
+        }
+    }
+
+    IEnumerator Shrink()
+    {
+        var cnt = 0;
+        while (col.bounds.size.z > 0.1 && cnt <= 1000)
+        {
+            print(col.bounds.size.z);
+            cnt++;
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * 0.99f);
+            yield return new WaitForSeconds(0.008f);
+        }
+        stateIsLarge = false;
+        canChange = true;
+        print("Shrink Finished");
+    }
+
+    IEnumerator Enlarge()
+    {
+        var cnt = 0;
+        while (transform.localScale.z < 1 && cnt <= 1000)
+        {
+            cnt++;
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z * 1.05f);
+            yield return new WaitForSeconds(0.008f);
+        }
+        stateIsLarge = true;
+        canChange = true;
+        print("Enlarge Finished");
+    }
 }
