b86d36f5a1e7959b924ae8b6124d9ea5247aaf43
王哲玮 <m13636472690@163.com>
2024-05-05 22:01:28 +0800

    update

---
+++ The Others/Assets/ResetObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetObject : MonoBehaviour
+{
+    public float maxY = 20;
+    private GameObject[] objects;
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
